428595c9ffe522a03aacb04bf8d444b95d768c54
oluwadamilarecugine <60507107+oluwadamilarecugine@users.noreply.github.com>
2020-07-17 23:45:55 +0100

    Add project files.

---
+++ DLCTest/PageObjets/QAEngineerTestPage.cs
@@ -0,0 +1,55 @@
+﻿using DLCTest.Hooks;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading;
+
+namespace DLCTest.PageObjets
+{
+    class QAEngineerTestPage
+    {
+        IWebDriver driver;
+
+        public QAEngineerTestPage()
+        {
+            driver = Hook.driver;
+        }
+        public void Navigatetopage(String url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        IWebElement enterUsername => driver.FindElement(By.XPath("//*[@type='text']"));
+        IWebElement enterPassword => driver.FindElement(By.XPath("//*[@type='password']"));
+        IWebElement clickOnLogin => driver.FindElement(By.XPath("//*[@type='submit']"));
+        IWebElement successfulMessage => driver.FindElement(By.XPath("//*[@class='logged-in__success']"));
+
+
+        public void EnterUsername(String username)
+        {
+            enterUsername.SendKeys(username);
+        }
+
+        public void EnterPassword(String password)
+        {
+            enterPassword.SendKeys(password);
+        }
+
+        public void ClickOnLogin()
+        {
+            clickOnLogin.Click();
+        }
+
+        public bool SuccessfulMessage(string successmessage)
+        {
+           
+            return successfulMessage.Displayed;
+        }
+
+
+
+
+    }
+}
---
+++ DLCTest/StepDefinations/QAEngineerTestSteps.cs
@@ -0,0 +1,59 @@
+﻿using DLCTest.PageObjets;
+using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace DLCTest
+{
+    [Binding]
+    public class QAEngineerTestSteps
+    {
+        QAEngineerTestPage qaEngineerTestPage;
+
+        public QAEngineerTestSteps()
+        {
+            qaEngineerTestPage = new QAEngineerTestPage();
+        }
+
+        [Given(@"I navigate to url ""(.*)""")]
+        public void GivenINavigateToUrl(string url)
+        {
+            qaEngineerTestPage.Navigatetopage(url);
+        }
+
+
+        [When(@"I enter username ""(.*)""")]
+        public void WhenIEnterUsername(string username)
+        {
+            qaEngineerTestPage.EnterUsername(username);
+        }
+
+        [When(@"I enter password ""(.*)""")]
+        public void WhenIEnterPassword(string password)
+        {
+            qaEngineerTestPage.EnterPassword(password);
+        }
+        
+        [When(@"I click on login")]
+        public void WhenIClickOnLogin()
+        {
+            qaEngineerTestPage.ClickOnLogin(); 
+        }
+        
+        [Then(@"Successful login message should display ""(.*)""")]
+        public void ThenSuccessfulLoginMessageFrameShouldDisplay(string successmessage)
+        {
+            Assert.True(qaEngineerTestPage.SuccessfulMessage(successmessage));
+        }
+
+        //InvalidUsername/passwordAssertion    
+
+        [Then(@"The message ""(.*)"" Should not display")]
+        public void ThenTheMessageShouldNotDisplay(string successmessage)
+        {
+            Assert.False(qaEngineerTestPage.SuccessfulMessage(successmessage));
+        }
+
+
+    }
+}
